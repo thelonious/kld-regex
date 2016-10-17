d0d0728c10b94f4d51a3f26763ad1ba52cabe850
Kevin Lindsey <kevin@kevlindev.com>
2016-10-16 23:12:43 -0700

    Show an example of typical usage

---
+++ test-simple.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+let Regex = require('./lib/Regex');
+
+let runner = Regex.parse("(abc)+");
+var source = "abcdefabc";
+var matches = runner.all(source);
+
+console.log("source = '%s'", source);
+runner.all(source).forEach(match => {
+	console.log(
+		"source[%s:%s] = %s",
+		match.startingOffset,
+		match.endingOffset,
+		match.text
+	);
+});
