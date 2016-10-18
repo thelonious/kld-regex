eb8d996333f179fd1a7442e02ae2fa76b8c233b6
Kevin Lindsey <kevin@kevlindev.com>
2016-10-17 21:16:06 -0700

    Expose DFARunner

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.Regex = require('./lib/Regex');
 exports.NFAGraphConverter = require('./lib/NFAGraphConverter');
+exports.DFARunner = require('./lib/DFARunner');
 exports.Lexer = require('./lib/Lexer');
